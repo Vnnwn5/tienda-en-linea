692e154af71bad2d1708fe96c3b0c20da4e11546
Valeria Aguila <aguila.vaam@gmail.com>
2021-05-05 15:08:43 -0700

    Manage  roles and permission

--- Config/Config.php
+++ Config/Config.php
@@ -8,7 +8,7 @@
 
     //Datos de conexion a base de datos
     const DB_HOST = "localhost";
-    const DB_NAME = "tienda";
+    const DB_NAME = "db_tiendavirtual";
     const DB_USER = "root";
     const DB_PASSWORD = "root";
     const DB_CHARSET = "charset=utf8";
