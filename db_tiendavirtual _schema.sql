318036aea4ffd124fee0d1e213bc6115c5aebaa5
Valeria Aguila <aguila.vaam@gmail.com>
2021-05-05 22:40:28 -0700

    db schema added

---
+++ db_tiendavirtual _schema.sql
@@ -0,0 +1,387 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 05, 2021 at 10:37 PM
+-- Server version: 8.0.23-0ubuntu0.20.04.1
+-- PHP Version: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_tiendavirtual`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` bigint NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `datecreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detalle_pedido`
+--
+
+CREATE TABLE `detalle_pedido` (
+  `id` bigint NOT NULL,
+  `pedidoid` bigint NOT NULL,
+  `productoid` bigint NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `cantidad` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detalle_temp`
+--
+
+CREATE TABLE `detalle_temp` (
+  `id` bigint NOT NULL,
+  `productoid` bigint NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `cantidad` int NOT NULL,
+  `token` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modulo`
+--
+
+CREATE TABLE `modulo` (
+  `idmodulo` bigint NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `status` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+--
+-- Dumping data for table `modulo`
+--
+
+INSERT INTO `modulo` (`idmodulo`, `titulo`, `descripcion`, `status`) VALUES
+(1, 'Dashboard', 'Dashboard', 1),
+(2, 'Usuarios', 'Usuarios del sistema', 1),
+(3, 'Clientes', 'Clientes de tienda', 1),
+(4, 'Productos', 'Todos los productos', 1),
+(5, 'Pedidos', 'Pedidos', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idpedido` bigint NOT NULL,
+  `personaid` bigint NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `monto` decimal(11,2) NOT NULL,
+  `tipopagoid` bigint NOT NULL,
+  `status` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `idpermiso` bigint NOT NULL,
+  `rolid` bigint NOT NULL,
+  `moduloid` bigint NOT NULL,
+  `r` int NOT NULL DEFAULT '0',
+  `w` int NOT NULL DEFAULT '0',
+  `u` int NOT NULL DEFAULT '0',
+  `d` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+--
+-- Dumping data for table `permisos`
+--
+
+INSERT INTO `permisos` (`idpermiso`, `rolid`, `moduloid`, `r`, `w`, `u`, `d`) VALUES
+(1, 7, 1, 0, 0, 0, 0),
+(2, 7, 2, 0, 0, 0, 0),
+(3, 7, 3, 1, 0, 0, 0),
+(4, 7, 4, 0, 0, 0, 0),
+(5, 7, 5, 0, 0, 0, 0),
+(26, 3, 1, 1, 0, 0, 0),
+(27, 3, 2, 0, 0, 0, 0),
+(28, 3, 3, 1, 1, 1, 0),
+(29, 3, 4, 1, 1, 1, 1),
+(30, 3, 5, 1, 1, 1, 0),
+(31, 2, 1, 1, 1, 1, 1),
+(32, 2, 2, 1, 1, 1, 1),
+(33, 2, 3, 1, 1, 1, 1),
+(34, 2, 4, 1, 1, 1, 1),
+(35, 2, 5, 1, 1, 1, 1),
+(41, 10, 1, 0, 1, 1, 1),
+(42, 10, 2, 0, 0, 0, 0),
+(43, 10, 3, 1, 1, 1, 1),
+(44, 10, 4, 1, 1, 1, 1),
+(45, 10, 5, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persona`
+--
+
+CREATE TABLE `persona` (
+  `idpersona` bigint NOT NULL,
+  `identificacion` varchar(30) NOT NULL,
+  `nombres` varchar(80) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `telefono` bigint NOT NULL,
+  `email_user` varchar(100) NOT NULL,
+  `password` varchar(75) CHARACTER SET dec8 COLLATE dec8_swedish_ci NOT NULL,
+  `nit` varchar(20) NOT NULL,
+  `nombrefiscal` varchar(80) NOT NULL,
+  `direccionfiscal` varchar(100) NOT NULL,
+  `token` varchar(80) NOT NULL,
+  `rolid` bigint NOT NULL,
+  `datecreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `producto`
+--
+
+CREATE TABLE `producto` (
+  `idproducto` bigint NOT NULL,
+  `categoriaid` bigint NOT NULL,
+  `codigo` varchar(30) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `stock` int NOT NULL,
+  `imagen` varchar(100) NOT NULL,
+  `datecreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rol`
+--
+
+CREATE TABLE `rol` (
+  `idrol` bigint NOT NULL,
+  `nombrerol` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `status` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=dec8;
+
+--
+-- Dumping data for table `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `nombrerol`, `descripcion`, `status`) VALUES
+(2, 'Encargados', 'Encargados', 1),
+(3, 'supervisores 6', 'supervisores', 1),
+(4, 'ejemplo 2 nuevo', 'ejemplo 2 nuevo', 0),
+(5, 'ejemplo 2', 'ejemplo 2', 0),
+(6, 'bodega', 'bodega', 1),
+(7, 'reporteria 11', 'reporteria 22', 1),
+(8, 'coordinador', 'coordinador', 0),
+(9, 'admin', 'admin', 1),
+(10, 'editor 12', 'editor', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Indexes for table `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pedidoid` (`pedidoid`),
+  ADD KEY `productoid` (`productoid`);
+
+--
+-- Indexes for table `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productoid` (`productoid`);
+
+--
+-- Indexes for table `modulo`
+--
+ALTER TABLE `modulo`
+  ADD PRIMARY KEY (`idmodulo`);
+
+--
+-- Indexes for table `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idpedido`),
+  ADD KEY `personaid` (`personaid`);
+
+--
+-- Indexes for table `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`idpermiso`),
+  ADD KEY `rolid` (`rolid`),
+  ADD KEY `moduloid` (`moduloid`);
+
+--
+-- Indexes for table `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`idpersona`),
+  ADD KEY `rolid` (`rolid`);
+
+--
+-- Indexes for table `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idproducto`),
+  ADD KEY `categoria` (`categoriaid`);
+
+--
+-- Indexes for table `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`idrol`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `modulo`
+--
+ALTER TABLE `modulo`
+  MODIFY `idmodulo` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idpedido` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `idpermiso` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT for table `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `idpersona` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idproducto` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `idrol` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD CONSTRAINT `detalle_pedido_ibfk_1` FOREIGN KEY (`pedidoid`) REFERENCES `pedido` (`idpedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle_pedido_ibfk_2` FOREIGN KEY (`productoid`) REFERENCES `producto` (`idproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  ADD CONSTRAINT `detalle_temp_ibfk_1` FOREIGN KEY (`productoid`) REFERENCES `producto` (`idproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`personaid`) REFERENCES `persona` (`idpersona`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `permisos`
+--
+ALTER TABLE `permisos`
+  ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`rolid`) REFERENCES `rol` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `permisos_ibfk_2` FOREIGN KEY (`moduloid`) REFERENCES `modulo` (`idmodulo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`rolid`) REFERENCES `rol` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoriaid`) REFERENCES `categoria` (`idcategoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
