174d82644b2eaaf9ad6704da3f62ddbd43e7717b
Valeria Aguila <aguila.vaam@gmail.com>
2021-05-20 13:10:55 -0700

    User profile design

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -170,4 +170,12 @@ header("Location:".base_url().'/dashboard');
         }
         die();
     }
+    public function perfil(){
+        $data['page_tag'] = "Perfil";
+        $data['page_title'] = "Perfil de usuario";
+        $data['page_name'] = "perfil";
+        $data['page_functions_js'] = "functions_usuarios.js";
+        $this->views->getView($this,"perfil",$data);
+    }
+
 }
--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -14,8 +14,6 @@
             <div class="title">
                 <div class="title-body">Dashboard</div>
                 <?php dep($_SESSION['userData']);
-                dep($_SESSION['permisos'] );
-                dep($_SESSION['permisosMod']);
                 ?>
             </div>
         </div>
