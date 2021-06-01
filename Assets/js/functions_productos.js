0ea4f5d73b97040b4a37e69dc95eb832505c6cca
Valeria Aguila <aguila.vaam@gmail.com>
2021-05-31 18:19:11 -0700

    Product system part 1

---
+++ Assets/js/functions_productos.js
@@ -0,0 +1,59 @@
+document.write(`<script src="${base_url}/Assets/js/plugins/JsBarcode.all.min.js"></script>`);
+
+
+$(document).on('focusin', function(e) {
+    if ($(e.target).closest(".tox-dialog").length) {
+        e.stopImmediatePropagation();
+    }
+});
+if(document.querySelector("#txtCodigo")){
+    let inputCodigo = document.querySelector("#txtCodigo");
+    inputCodigo.onkeyup = function() {
+        if(inputCodigo.value.length >= 5){
+            document.querySelector('#divBarCode').classList.remove("notblock");
+            fntBarcode();
+        }else{
+            document.querySelector('#divBarCode').classList.add("notblock");
+        }
+    };
+}
+
+tinymce.init({
+    selector: '#txtDescripcion',
+    width: "100%",
+    height: 400,
+    statusbar: true,
+    plugins: [
+        "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+        "save table contextmenu directionality emoticons template paste textcolor"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
+});
+function fntBarcode(){
+    let codigo = document.querySelector("#txtCodigo").value;
+    JsBarcode("#barcode", codigo);
+}
+function fntPrintBarcode(area){
+    let elemntArea = document.querySelector(area);
+    let vprint = window.open(' ', 'popimpr', 'height=400,width=600');
+    vprint.document.write(elemntArea.innerHTML );
+    vprint.document.close();
+    vprint.print();
+    vprint.close();
+}
+function openModal()
+{
+   // rowTable = "";
+    document.querySelector('#idProducto').value ="";
+    document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
+    document.querySelector('#btnActionForm').classList.replace("btn-info", "btn-primary");
+    document.querySelector('#btnText').innerHTML ="Guardar";
+    document.querySelector('#titleModal').innerHTML = "Nuevo Producto";
+    document.querySelector("#formProductos").reset();
+    //document.querySelector("#divBarCode").classList.add("notblock");
+   // document.querySelector("#containerGallery").classList.add("notblock");
+   // document.querySelector("#containerImages").innerHTML = "";
+    $('#modalFormProductos').modal('show');
+
+}
